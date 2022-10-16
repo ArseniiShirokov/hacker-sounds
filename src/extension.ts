a2b020c276f12263daac7399f3e7164bbff08383
Арсений Широков <arsenii.shirokov@tevian.ru>
2022-10-16 19:24:31 +0400

    add change speed rate 1.0

--- src/extension.ts
+++ src/extension.ts
@@ -6,6 +6,7 @@ import * as path from 'path';
 import player, { PlayerConfig } from './player';
 import debounce = require('lodash.debounce');
 import { toInteger } from 'lodash';
+import { Transform } from 'stream';
 
 let listener: EditorListener;
 let isActive: boolean;
@@ -15,8 +16,14 @@ let config: PlayerConfig = {
     winVol: 100,
     linuxVol: 100
 };
+let start: number;
+let mpv = require('node-mpv');
+let mpvPlayer = new mpv();
+
+const stat_period_duration = 5 * 1000;
 
 export function activate(context: vscode.ExtensionContext) {
+    start = Date.now();
     console.log('Initializing "hacker-sounds" extension');
 
     // is the extension activated? yes by default.
@@ -227,19 +234,13 @@ export function deactivate() { }
  * Listen to editor changes and play a sound when a key is pressed.
  */
 export class EditorListener {
+    private _current_temp = 0;
+    private _current_period = 0;
+    private _music_started = false;
+
     private _disposable: vscode.Disposable;
     private _subscriptions: vscode.Disposable[] = [];
     private _basePath: string = path.join(__dirname, '..');
-
-    // Audio files
-    private _spaceAudio: string = path.join(this._basePath, 'audio', 'spacebar.wav');
-    private _deleteAudio: string = path.join(this._basePath, 'audio', 'delete.wav');
-    private _otherKeysAudio: string = path.join(this._basePath, 'audio', 'key.wav');
-    private _cutAudio: string = path.join(this._basePath, 'audio', 'cut.wav');
-    private _pasteAudio: string = path.join(this._basePath, 'audio', 'paste.wav');
-    private _enterAudio: string = path.join(this._basePath, 'audio', 'enter.wav');
-    private _tabAudio: string = path.join(this._basePath, 'audio', 'tab.wav');
-    private _arrowsAudio: string = path.join(this._basePath, 'audio', 'arrow.wav');
 
     constructor(private player: any) {
         isNotArrowKey = false;
@@ -252,82 +253,56 @@ export class EditorListener {
         };
     }
 
+    map_temp_to_interval(temp: number) {
+        if (temp < 20) {
+            return 0.75
+        } else if (temp < 30) {
+            return 1;
+        } else if (temp < 40) {
+            return 1.25;
+        } else {
+            return 1.5;
+        }
+    }
+
+    adjust_speed() {
+        if (!this._music_started) {
+            this._music_started = true;
+            mpvPlayer.load("https://soundcloud.com/90sflav/callme?in=user-692461400/sets/lofi");
+        }
+        // Check of starting new stat period
+        let new_period = Math.round((Date.now() - start) / stat_period_duration);
+
+        if (new_period != this._current_period) {
+            let scale = this.map_temp_to_interval(this._current_temp);
+            console.log("Temp " + this._current_temp);
+            console.log("Scale" + scale);
+            console.log("    ");
+            mpvPlayer.speed(scale);
+            this._current_period = new_period;
+            this._current_temp = 0
+        } else {
+            this._current_temp = this._current_temp + 1;
+        }
+    }
+
     _keystrokeCallback = debounce((event: vscode.TextDocumentChangeEvent) => {
         if (!isActive) { return; }
-
         let activeDocument = vscode.window.activeTextEditor && vscode.window.activeTextEditor.document;
         if (event.document !== activeDocument || event.contentChanges.length === 0) { return; }
-
-        isNotArrowKey = true;
-        let pressedKey = event.contentChanges[0].text;
-
-        switch (pressedKey) {
-            case '':
-                if (event.contentChanges[0].rangeLength === 1) {
-                    // backspace or delete pressed
-                    this.player.play(this._deleteAudio);
-                } else {
-                    // text cut
-                    this.player.play(this._cutAudio);
-                }
-                break;
-
-            case ' ':
-                // space bar pressed
-                this.player.play(this._spaceAudio);
-                break;
-
-            case '\n':
-                // enter pressed
-                this.player.play(this._enterAudio);
-                break;
-
-            case '\t':
-            case '  ':
-            case '    ':
-                // tab pressed
-                this.player.play(this._tabAudio);
-                break;
-
-            default:
-                let textLength = pressedKey.trim().length;
-
-                switch (textLength) {
-                    case 0:
-                        // user hit Enter while indented
-                        this.player.play(this._enterAudio);
-                        break;
-
-                    case 1:
-                        // it's a regular character
-                        this.player.play(this._otherKeysAudio);
-                        break;
-
-                    default:
-                        // text pasted
-                        this.player.play(this._pasteAudio);
-                        break;
-                }
-                break;
-        }
+        this.adjust_speed();
     }, 100, { leading: true });
 
     _arrowKeysCallback = debounce((event: vscode.TextEditorSelectionChangeEvent) => {
         if (!isActive) { return; }
-
         // current editor
         const editor = vscode.window.activeTextEditor;
         if (!editor || editor.document !== event.textEditor.document) { return; }
-
-        // check if there is no selection
-        if (editor.selection.isEmpty && isNotArrowKey === false) {
-            this.player.play(this._arrowsAudio);
-        } else {
-            isNotArrowKey = false;
-        }
+        this.adjust_speed();
     }, 100, { leading: true });
 
     dispose() {
         this._disposable.dispose();
+        mpvPlayer.stop();
     }
 }
